28b793ef79b5034d7996dbc6bc808fda3da561c1
Brian McKenna <bmckenna781@gmail.com>
2016-09-23 18:37:05 -0400

    Added OpenGL Components
    SharpGL 2.4 and SharpGL.WPF 2.4

--- PlateTracker.UI/PlateTracker.UI/Views/MainView.xaml.cs
+++ PlateTracker.UI/PlateTracker.UI/Views/MainView.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using PlateTracker.UI.ViewModels;
 using PlateTracker.UI.Views;
+using SharpGL;
+using SharpGL.SceneGraph;
 
 namespace PlateTracker.UI.Views
 {
@@ -25,6 +27,8 @@ namespace PlateTracker.UI.Views
         private bool _drawMode = false;
         private Point _startPoint;
         private Line _activeLine;
+        //float rotatePyramid = 0;
+        float rquad = 0;
 
         public MainView(MainVM vm)
         {
@@ -92,5 +96,92 @@ namespace PlateTracker.UI.Views
             DrawImage.Children.Add(rect);
 
         }
+
+        #region OpenGL
+        private void OpenGLControl_OnOpenGLInitialized(object sender, OpenGLEventArgs args)
+        {
+            OpenGL gl = openGLControl.OpenGL;
+        }
+
+        private void OpenGLControl_OnResized(object sender, OpenGLEventArgs args)
+        {
+            // Get the OpenGL instance.
+            OpenGL gl = openGLControl.OpenGL;
+            // Load and clear the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            gl.LoadIdentity();
+            // Calculate The Aspect Ratio Of The Window
+            gl.Perspective(45.0f, (float)gl.RenderContextProvider.Width / (float)gl.RenderContextProvider.Height,
+            0.1f, 100.0f);
+            // Load the modelview.
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+        }
+
+        private void OpenGLControl_OnOpenGLDraw(object sender, EventArgs args)
+        {
+            //  Get the OpenGL instance that's been passed to us.
+            OpenGL gl = openGLControl.OpenGL;
+
+            //  Clear the color and depth buffers.
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            //  Reset the modelview matrix.
+            //gl.LoadIdentity();
+
+            //  Move the geometry into a fairly central position.
+            //gl.Translate(-1.5f, 0.0f, -6.0f);
+
+
+
+
+            //  Reset the modelview.
+            gl.LoadIdentity();
+            //  Move into a more central position.
+            gl.Translate(1.5f, 0.0f, -12.0f);
+            //  Rotate the cube.
+            gl.Rotate(rquad, 1.0f, 1.0f, 1.0f);
+
+
+            //  Provide the cube colors and geometry.
+            gl.Begin(OpenGL.GL_QUADS);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+            gl.Color(1.0f, 0.5f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Color(1.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, 1.0f);
+            gl.Vertex(-1.0f, 1.0f, -1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Color(1.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, 1.0f, -1.0f);
+            gl.Vertex(1.0f, 1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.End();
+            //  Flush OpenGL.
+            gl.Flush();
+            //  Rotate the geometry a bit.
+            //rotatePyramid += 3.0f;
+            rquad -= 3.0f;
+        }
+
+        #endregion
     }
 }
