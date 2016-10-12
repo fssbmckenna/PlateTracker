1de3be31a71422e3e43935f2e6d5fa6447c7a2a1
Brian McKenna <bmckenna781@gmail.com>
2016-10-12 19:42:40 -0400

    Ongoing OpenGL testing

--- PlateTracker.UI/PlateTracker.UI/ViewModels/MainVM.cs
+++ PlateTracker.UI/PlateTracker.UI/ViewModels/MainVM.cs
@@ -193,6 +193,41 @@ namespace PlateTracker.UI.ViewModels
 
             Application.Current.Windows[0].Close();
         }
+
+        private void CreateOpenGLObject()
+        {
+            /****************************************************************************************
+            https://www.opengl.org/discussion_boards/showthread.php/177839-OpenGL-vertex-arrays-in-C
+
+            There are two major different ways to use OpenGL.The deprecated old way, before OpenGL 3, was the fixed function pipeline. 
+            This one is easier to use and understand. You do a glBegin(), define a number of vertices with glVertex() / glNormal() / glTexCoord(), 
+            and end it all with glEnd().Using glVertexPointer() is a more efficient way to do this.
+
+            But that was the old ways. If you search for tutorials, you will find a lot that use this old style.The new way(since 2008!),
+            is to define all your vertex data in buffers that you transfer to the GPU and then write your own shader.Now, you instead use 
+            glBufferData()(for example) to transfer data to the GPU, and glVertexAttribPointer() to define the layout if this data.
+            This one takes all information about each vertex at the same time (coordinates, normals, texture coordinates, and anything else
+            you like).But that means you have to write your own shader, which is the program that executes on the GPU.That is the program 
+            that is going to interpret your data. (It was possible to use your own shader before OpenGL 3).
+
+            Now a little about the stride.Suppose you have, in total, 32 bytes of data for each vertex (called vertex attrib data), in a buffer. 
+            If that buffer is packed, which means that there are no extra bytes between one record and the next, then you can either set the stride 
+            to 0 or to 32.But if your data is, for example, 31 bytes, it will be organized in memory on a 32 byte boundary, with 1 byte as filler. 
+            Now you have to define the stride as 32, even though you only have 31 bytes of actual data for each vertex. When programming in C / C++, 
+            it is very easy to have full control and understanding of memory allocation for this type of data.I don't know how to use OpenGL from C#, 
+            so this you will have to find elsewhere. Or use a language better adapted for OpenGL.
+
+
+            For a nice tutorial, that also gives good information about 3D programming, see http://www.arcsynthesis.org/gltut/.
+            Quote Originally Posted by fiodis  View Post
+            On a slightly related note, the reference pages for OpenGL aren't working for me; they just open a new tab which never loads completely. 
+            I'm using IE9, so according to the preface they ought to be compatible. Is there something I'm missing, or is there some other place where
+            I might access them?
+            
+            Try using http://www.opengl.org/wiki/Category:Core_API_Reference instead, but it will not show deprecated functions.
+
+    */
+        }
         #endregion
         #endregion
 
--- PlateTracker.UI/PlateTracker.UI/Views/MainView.xaml.cs
+++ PlateTracker.UI/PlateTracker.UI/Views/MainView.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Tracing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,9 +20,22 @@ using PlateTracker.UI.ViewModels;
 using PlateTracker.UI.Views;
 using SharpGL;
 using SharpGL.SceneGraph;
+using SharpGL.SceneGraph.Shaders;
+using SharpGL.VertexBuffers;
+using ShaderProgram = SharpGL.Shaders.ShaderProgram;
 
 namespace PlateTracker.UI.Views
 {
+
+    public class Square
+    {
+        public VertexBufferArray[] Vertices;
+
+        public Square()
+        {
+            Vertices = new VertexBufferArray[4];
+        }
+    }
    
     /// <summary>
     /// Interaction logic for MainView.xaml
@@ -136,13 +152,100 @@ namespace PlateTracker.UI.Views
             if (_type == "P")
             {
                 //show a pyramid
-                OpenGLControl_DrawPyramid(sender, args);
+                //OpenGLControl_DrawPyramid(sender, args);
+                OpenGLControl_Triangle(sender, args);
             }
             else
             {
                 //show a cube
                 OpenGLControl_DrawCube(sender, args);
             }
+            
+        }
+
+        
+
+        private void OpenGLControl_Triangle(object sender, EventArgs args)
+        {
+            Square[] side = new Square[6];
+            for (var i = 0; i < 6; i++)
+            {
+                side[i] = new Square();
+            }
+
+            side[0].Vertices[0] = new VertexBufferArray();
+
+
+            OpenGL gl = openGLControl.OpenGL;
+            //  Clear the color and depth buffers.
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            float[] colors = new float[9];
+            ShaderProgram program;
+            float[] trianglePoints =
+            {
+                0.0f, 0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f,
+                -0.5f, -0.5f, 0.0f
+            };
+
+            SolidColorBrush solidColorBrush = new SolidColorBrush();
+            solidColorBrush.Color = Color.FromRgb(100,50,20);
+            IntPtr colorsPtr;
+            IntPtr trianglePointsPtr;
+            VertexBufferArray vertexBufferArray;
+
+            for (var i = 0; i < colors.Length; i += 3)
+            {
+                colors[i] = solidColorBrush.Color.R/255.0f;
+                colors[i + 1] = solidColorBrush.Color.G/255.0f;
+                colors[i + 1] = solidColorBrush.Color.B/255.0f;
+            }
+
+            vertexBufferArray = new VertexBufferArray();
+            vertexBufferArray.Create(gl);
+            vertexBufferArray.Bind(gl);
+
+            var colorsVboArray = new uint[1];
+            gl.GenBuffers(1, colorsVboArray);
+            gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, colorsVboArray[0]);
+            colorsPtr = GCHandle.Alloc(colors, GCHandleType.Pinned).AddrOfPinnedObject();
+            gl.BufferData(OpenGL.GL_ARRAY_BUFFER, colors.Length*Marshal.SizeOf<float>(),
+                colorsPtr, OpenGL.GL_STATIC_DRAW);
+
+            var triangleVboArray = new uint[1];
+            gl.GenBuffers(1, triangleVboArray);
+            gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, triangleVboArray[0]);
+            trianglePointsPtr = GCHandle.Alloc(trianglePoints, GCHandleType.Pinned).AddrOfPinnedObject();
+            gl.BufferData(OpenGL.GL_ARRAY_BUFFER, trianglePoints.Length*Marshal.SizeOf<float>(),
+                trianglePointsPtr, OpenGL.GL_STATIC_DRAW);
+
+            gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, triangleVboArray[0]);
+            gl.VertexAttribPointer(0,3,OpenGL.GL_FLOAT, false, 0, IntPtr.Zero);
+            gl.BindBuffer(OpenGL.GL_ARRAY_BUFFER, colorsVboArray[0]);
+            gl.VertexAttribPointer(1,3, OpenGL.GL_FLOAT, false, 0, IntPtr.Zero);
+
+            gl.EnableVertexAttribArray(0);
+            gl.EnableVertexAttribArray(1);
+
+            var vertexShader = new FragmentShader();
+            vertexShader.CreateInContext(gl);
+            vertexShader.SetSource(new StreamReader(Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream(@"OpenGLTest.Shaders.Background.SolidColor.SolidColorVertex.glsl"))
+                .ReadToEnd());
+            vertexShader.Compile();
+
+            var fragmentShader = new FragmentShader();
+            fragmentShader.CreateInContext(gl);
+            fragmentShader.SetSource(new StreamReader(
+                    Assembly.GetExecutingAssembly()
+                        .GetManifestResourceStream(@"OpenGLTest.Shaders.Background.SolidColor.SolidColorFragment.glsl"))
+                .ReadToEnd());
+            fragmentShader.Compile();
+
+            program = new ShaderProgram();
+            program.Create(gl, @"OpenGLTest.Shaders.Background.SolidColor.SolidColorVertex.glsl", @"OpenGLTest.Shaders.Background.SolidColor.SolidColorFragment.glsl",null);
+            
             
         }
 
@@ -212,6 +315,7 @@ namespace PlateTracker.UI.Views
 
             //  Provide the cube colors and geometry.
             gl.Begin(OpenGL.GL_QUADS);
+          
             gl.Color(0.0f, 1.0f, 0.0f);
             gl.Vertex(1.0f, 1.0f, -1.0f);
             gl.Vertex(-1.0f, 1.0f, -1.0f);
